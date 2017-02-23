95109f2f7e929566d8f7cc795427e65b77925881
Franz-Josef Haider <f_haider@gmx.at>
2017-02-23 14:54:17 +0000

    (hybris) Allow libc.so to be initialized from libhybris
    libhybris then has to be built with --enable-initialize-bionic

--- libc/include/pthread.h
+++ libc/include/pthread.h
@@ -36,6 +36,12 @@
 #include <sys/types.h>
 #include <time.h>
 
+#ifndef HYBRIS
+#define ATTRIBUTE_WEAK
+#else
+#define ATTRIBUTE_WEAK __attribute__((weak))
+#endif
+
 typedef struct {
 #if defined(__LP64__)
   int32_t __private[10];
@@ -113,108 +119,108 @@ typedef int pthread_once_t;
 
 __BEGIN_DECLS
 
-int pthread_atfork(void (*)(void), void (*)(void), void(*)(void));
+int ATTRIBUTE_WEAK pthread_atfork(void (*)(void), void (*)(void), void(*)(void));
 
-int pthread_attr_destroy(pthread_attr_t*) __nonnull((1));
-int pthread_attr_getdetachstate(const pthread_attr_t*, int*) __nonnull((1, 2));
-int pthread_attr_getguardsize(const pthread_attr_t*, size_t*) __nonnull((1, 2));
-int pthread_attr_getschedparam(const pthread_attr_t*, struct sched_param*) __nonnull((1, 2));
-int pthread_attr_getschedpolicy(const pthread_attr_t*, int*) __nonnull((1, 2));
-int pthread_attr_getscope(const pthread_attr_t*, int*) __nonnull((1, 2));
-int pthread_attr_getstack(const pthread_attr_t*, void**, size_t*) __nonnull((1, 2, 3));
-int pthread_attr_getstacksize(const pthread_attr_t*, size_t*) __nonnull((1, 2));
-int pthread_attr_init(pthread_attr_t*) __nonnull((1));
-int pthread_attr_setdetachstate(pthread_attr_t*, int) __nonnull((1));
-int pthread_attr_setguardsize(pthread_attr_t*, size_t) __nonnull((1));
-int pthread_attr_setschedparam(pthread_attr_t*, const struct sched_param*) __nonnull((1, 2));
-int pthread_attr_setschedpolicy(pthread_attr_t*, int) __nonnull((1));
-int pthread_attr_setscope(pthread_attr_t*, int) __nonnull((1));
-int pthread_attr_setstack(pthread_attr_t*, void*, size_t) __nonnull((1));
-int pthread_attr_setstacksize(pthread_attr_t*, size_t stack_size) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_attr_destroy(pthread_attr_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_attr_getdetachstate(const pthread_attr_t*, int*) __nonnull((1, 2));
+int ATTRIBUTE_WEAK pthread_attr_getguardsize(const pthread_attr_t*, size_t*) __nonnull((1, 2));
+int ATTRIBUTE_WEAK pthread_attr_getschedparam(const pthread_attr_t*, struct sched_param*) __nonnull((1, 2));
+int ATTRIBUTE_WEAK pthread_attr_getschedpolicy(const pthread_attr_t*, int*) __nonnull((1, 2));
+int ATTRIBUTE_WEAK pthread_attr_getscope(const pthread_attr_t*, int*) __nonnull((1, 2));
+int ATTRIBUTE_WEAK pthread_attr_getstack(const pthread_attr_t*, void**, size_t*) __nonnull((1, 2, 3));
+int ATTRIBUTE_WEAK pthread_attr_getstacksize(const pthread_attr_t*, size_t*) __nonnull((1, 2));
+int ATTRIBUTE_WEAK pthread_attr_init(pthread_attr_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_attr_setdetachstate(pthread_attr_t*, int) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_attr_setguardsize(pthread_attr_t*, size_t) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_attr_setschedparam(pthread_attr_t*, const struct sched_param*) __nonnull((1, 2));
+int ATTRIBUTE_WEAK pthread_attr_setschedpolicy(pthread_attr_t*, int) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_attr_setscope(pthread_attr_t*, int) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_attr_setstack(pthread_attr_t*, void*, size_t) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_attr_setstacksize(pthread_attr_t*, size_t stack_size) __nonnull((1));
 
-int pthread_condattr_destroy(pthread_condattr_t*) __nonnull((1));
-int pthread_condattr_getclock(const pthread_condattr_t*, clockid_t*) __nonnull((1, 2));
-int pthread_condattr_getpshared(const pthread_condattr_t*, int*) __nonnull((1, 2));
-int pthread_condattr_init(pthread_condattr_t*) __nonnull((1));
-int pthread_condattr_setclock(pthread_condattr_t*, clockid_t) __nonnull((1));
-int pthread_condattr_setpshared(pthread_condattr_t*, int) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_condattr_destroy(pthread_condattr_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_condattr_getclock(const pthread_condattr_t*, clockid_t*) __nonnull((1, 2));
+int ATTRIBUTE_WEAK pthread_condattr_getpshared(const pthread_condattr_t*, int*) __nonnull((1, 2));
+int ATTRIBUTE_WEAK pthread_condattr_init(pthread_condattr_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_condattr_setclock(pthread_condattr_t*, clockid_t) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_condattr_setpshared(pthread_condattr_t*, int) __nonnull((1));
 
-int pthread_cond_broadcast(pthread_cond_t*) __nonnull((1));
-int pthread_cond_destroy(pthread_cond_t*) __nonnull((1));
-int pthread_cond_init(pthread_cond_t*, const pthread_condattr_t*) __nonnull((1));
-int pthread_cond_signal(pthread_cond_t*) __nonnull((1));
-int pthread_cond_timedwait(pthread_cond_t*, pthread_mutex_t*, const struct timespec*) __nonnull((1, 2, 3));
-int pthread_cond_wait(pthread_cond_t*, pthread_mutex_t*) __nonnull((1, 2));
+int ATTRIBUTE_WEAK pthread_cond_broadcast(pthread_cond_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_cond_destroy(pthread_cond_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_cond_init(pthread_cond_t*, const pthread_condattr_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_cond_signal(pthread_cond_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_cond_timedwait(pthread_cond_t*, pthread_mutex_t*, const struct timespec*) __nonnull((1, 2, 3));
+int ATTRIBUTE_WEAK pthread_cond_wait(pthread_cond_t*, pthread_mutex_t*) __nonnull((1, 2));
 
-int pthread_create(pthread_t*, pthread_attr_t const*, void *(*)(void*), void*) __nonnull((1, 3));
-int pthread_detach(pthread_t);
+int ATTRIBUTE_WEAK pthread_create(pthread_t*, pthread_attr_t const*, void *(*)(void*), void*) __nonnull((1, 3));
+int ATTRIBUTE_WEAK pthread_detach(pthread_t);
 void pthread_exit(void*) __noreturn;
 
-int pthread_equal(pthread_t, pthread_t);
+int ATTRIBUTE_WEAK pthread_equal(pthread_t, pthread_t);
 
-int pthread_getattr_np(pthread_t, pthread_attr_t*) __nonnull((2));
+int ATTRIBUTE_WEAK pthread_getattr_np(pthread_t, pthread_attr_t*) __nonnull((2));
 
-int pthread_getcpuclockid(pthread_t, clockid_t*) __nonnull((2));
+int ATTRIBUTE_WEAK pthread_getcpuclockid(pthread_t, clockid_t*) __nonnull((2));
 
-int pthread_getschedparam(pthread_t, int*, struct sched_param*) __nonnull((2, 3));
+int ATTRIBUTE_WEAK pthread_getschedparam(pthread_t, int*, struct sched_param*) __nonnull((2, 3));
 
-void* pthread_getspecific(pthread_key_t);
+void* ATTRIBUTE_WEAK pthread_getspecific(pthread_key_t);
 
-pid_t pthread_gettid_np(pthread_t);
+pid_t ATTRIBUTE_WEAK pthread_gettid_np(pthread_t);
 
-int pthread_join(pthread_t, void**);
+int ATTRIBUTE_WEAK pthread_join(pthread_t, void**);
 
-int pthread_key_create(pthread_key_t*, void (*)(void*)) __nonnull((1));
-int pthread_key_delete(pthread_key_t);
+int ATTRIBUTE_WEAK pthread_key_create(pthread_key_t*, void (*)(void*)) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_key_delete(pthread_key_t);
 
-int pthread_mutexattr_destroy(pthread_mutexattr_t*) __nonnull((1));
-int pthread_mutexattr_getpshared(const pthread_mutexattr_t*, int*) __nonnull((1, 2));
-int pthread_mutexattr_gettype(const pthread_mutexattr_t*, int*) __nonnull((1, 2));
-int pthread_mutexattr_init(pthread_mutexattr_t*) __nonnull((1));
-int pthread_mutexattr_setpshared(pthread_mutexattr_t*, int) __nonnull((1));
-int pthread_mutexattr_settype(pthread_mutexattr_t*, int) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_mutexattr_destroy(pthread_mutexattr_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_mutexattr_getpshared(const pthread_mutexattr_t*, int*) __nonnull((1, 2));
+int ATTRIBUTE_WEAK pthread_mutexattr_gettype(const pthread_mutexattr_t*, int*) __nonnull((1, 2));
+int ATTRIBUTE_WEAK pthread_mutexattr_init(pthread_mutexattr_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_mutexattr_setpshared(pthread_mutexattr_t*, int) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_mutexattr_settype(pthread_mutexattr_t*, int) __nonnull((1));
 
-int pthread_mutex_destroy(pthread_mutex_t*) __nonnull((1));
-int pthread_mutex_init(pthread_mutex_t*, const pthread_mutexattr_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_mutex_destroy(pthread_mutex_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_mutex_init(pthread_mutex_t*, const pthread_mutexattr_t*) __nonnull((1));
 #if !defined(__LP64__)
-int pthread_mutex_lock(pthread_mutex_t*) /* __nonnull((1)) */;
+int ATTRIBUTE_WEAK pthread_mutex_lock(pthread_mutex_t*) /* __nonnull((1)) */;
 #else
-int pthread_mutex_lock(pthread_mutex_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_mutex_lock(pthread_mutex_t*) __nonnull((1));
 #endif
-int pthread_mutex_timedlock(pthread_mutex_t*, const struct timespec*) __nonnull((1, 2));
-int pthread_mutex_trylock(pthread_mutex_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_mutex_timedlock(pthread_mutex_t*, const struct timespec*) __nonnull((1, 2));
+int ATTRIBUTE_WEAK pthread_mutex_trylock(pthread_mutex_t*) __nonnull((1));
 #if !defined(__LP4__)
-int pthread_mutex_unlock(pthread_mutex_t*) /* __nonnull((1)) */;
+int ATTRIBUTE_WEAK pthread_mutex_unlock(pthread_mutex_t*) /* __nonnull((1)) */;
 #else
-int pthread_mutex_unlock(pthread_mutex_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_mutex_unlock(pthread_mutex_t*) __nonnull((1));
 #endif
 
-int pthread_once(pthread_once_t*, void (*)(void)) __nonnull((1, 2));
+int ATTRIBUTE_WEAK pthread_once(pthread_once_t*, void (*)(void)) __nonnull((1, 2));
 
-int pthread_rwlockattr_init(pthread_rwlockattr_t*) __nonnull((1));
-int pthread_rwlockattr_destroy(pthread_rwlockattr_t*) __nonnull((1));
-int pthread_rwlockattr_getpshared(const pthread_rwlockattr_t*, int*) __nonnull((1, 2));
-int pthread_rwlockattr_setpshared(pthread_rwlockattr_t*, int) __nonnull((1));
-int pthread_rwlockattr_getkind_np(const pthread_rwlockattr_t*, int*) __nonnull((1, 2));
-int pthread_rwlockattr_setkind_np(pthread_rwlockattr_t*, int) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_rwlockattr_init(pthread_rwlockattr_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_rwlockattr_destroy(pthread_rwlockattr_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_rwlockattr_getpshared(const pthread_rwlockattr_t*, int*) __nonnull((1, 2));
+int ATTRIBUTE_WEAK pthread_rwlockattr_setpshared(pthread_rwlockattr_t*, int) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_rwlockattr_getkind_np(const pthread_rwlockattr_t*, int*) __nonnull((1, 2));
+int ATTRIBUTE_WEAK pthread_rwlockattr_setkind_np(pthread_rwlockattr_t*, int) __nonnull((1));
 
-int pthread_rwlock_destroy(pthread_rwlock_t*) __nonnull((1));
-int pthread_rwlock_init(pthread_rwlock_t*, const pthread_rwlockattr_t*) __nonnull((1));
-int pthread_rwlock_rdlock(pthread_rwlock_t*) __nonnull((1));
-int pthread_rwlock_timedrdlock(pthread_rwlock_t*, const struct timespec*) __nonnull((1, 2));
-int pthread_rwlock_timedwrlock(pthread_rwlock_t*, const struct timespec*) __nonnull((1, 2));
-int pthread_rwlock_tryrdlock(pthread_rwlock_t*) __nonnull((1));
-int pthread_rwlock_trywrlock(pthread_rwlock_t*) __nonnull((1));
-int pthread_rwlock_unlock(pthread_rwlock_t *rwlock) __nonnull((1));
-int pthread_rwlock_wrlock(pthread_rwlock_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_rwlock_destroy(pthread_rwlock_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_rwlock_init(pthread_rwlock_t*, const pthread_rwlockattr_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_rwlock_rdlock(pthread_rwlock_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_rwlock_timedrdlock(pthread_rwlock_t*, const struct timespec*) __nonnull((1, 2));
+int ATTRIBUTE_WEAK pthread_rwlock_timedwrlock(pthread_rwlock_t*, const struct timespec*) __nonnull((1, 2));
+int ATTRIBUTE_WEAK pthread_rwlock_tryrdlock(pthread_rwlock_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_rwlock_trywrlock(pthread_rwlock_t*) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_rwlock_unlock(pthread_rwlock_t *rwlock) __nonnull((1));
+int ATTRIBUTE_WEAK pthread_rwlock_wrlock(pthread_rwlock_t*) __nonnull((1));
 
 pthread_t pthread_self(void) __pure2;
 
-int pthread_setname_np(pthread_t, const char*) __nonnull((2));
+int ATTRIBUTE_WEAK pthread_setname_np(pthread_t, const char*) __nonnull((2));
 
-int pthread_setschedparam(pthread_t, int, const struct sched_param*) __nonnull((3));
+int ATTRIBUTE_WEAK pthread_setschedparam(pthread_t, int, const struct sched_param*) __nonnull((3));
 
-int pthread_setspecific(pthread_key_t, const void*);
+int ATTRIBUTE_WEAK pthread_setspecific(pthread_key_t, const void*);
 
 typedef void (*__pthread_cleanup_func_t)(void*);
 
@@ -248,14 +254,14 @@ extern void __pthread_cleanup_pop(__pthread_cleanup_t*, int);
 // Bionic additions that are deprecated even in the 32-bit ABI.
 //
 // TODO: Remove them once chromium_org / NFC have switched over.
-int pthread_cond_timedwait_monotonic_np(pthread_cond_t*, pthread_mutex_t*, const struct timespec*);
-int pthread_cond_timedwait_monotonic(pthread_cond_t*, pthread_mutex_t*, const struct timespec*);
+int ATTRIBUTE_WEAK pthread_cond_timedwait_monotonic_np(pthread_cond_t*, pthread_mutex_t*, const struct timespec*);
+int ATTRIBUTE_WEAK pthread_cond_timedwait_monotonic(pthread_cond_t*, pthread_mutex_t*, const struct timespec*);
 
-int pthread_cond_timedwait_relative_np(pthread_cond_t*, pthread_mutex_t*, const struct timespec*) /* TODO: __attribute__((deprecated("use pthread_cond_timedwait instead")))*/;
+int ATTRIBUTE_WEAK pthread_cond_timedwait_relative_np(pthread_cond_t*, pthread_mutex_t*, const struct timespec*) /* TODO: __attribute__((deprecated("use pthread_cond_timedwait instead")))*/;
 #define HAVE_PTHREAD_COND_TIMEDWAIT_RELATIVE 1 /* TODO: stop defining this to push LP32 off this API sooner. */
-int pthread_cond_timeout_np(pthread_cond_t*, pthread_mutex_t*, unsigned) /* TODO: __attribute__((deprecated("use pthread_cond_timedwait instead")))*/;
+int ATTRIBUTE_WEAK pthread_cond_timeout_np(pthread_cond_t*, pthread_mutex_t*, unsigned) /* TODO: __attribute__((deprecated("use pthread_cond_timedwait instead")))*/;
 
-int pthread_mutex_lock_timeout_np(pthread_mutex_t*, unsigned) __attribute__((deprecated("use pthread_mutex_timedlock instead")));
+int ATTRIBUTE_WEAK pthread_mutex_lock_timeout_np(pthread_mutex_t*, unsigned) __attribute__((deprecated("use pthread_mutex_timedlock instead")));
 
 #endif /* !defined(__LP64__) */
 
